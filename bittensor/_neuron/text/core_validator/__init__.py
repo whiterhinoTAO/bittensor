0daa43e01239ecb2334aaf5eba50c402de5da974
opentaco <opentaco@protonmail.com>
2022-06-16 17:52:08 +0200

    Change table to moving average stats
    In validator epoch display.

--- bittensor/_neuron/text/core_validator/__init__.py
+++ bittensor/_neuron/text/core_validator/__init__.py
@@ -317,9 +317,9 @@ class neuron:
             current_block = self.subtensor.block
             step_time = time.time() - start_time
 
-            # === Stats table (step) ===
+            # === Stats table (avg) ===
             table = Table(width=self.config.get('width', None), pad_edge=False, box=None)
-            table.title = f'[white]Validator (stats)[/white] | [bold]UID {self.uid}[/bold] ' \
+            table.title = f'[white]Stats update[/white] | [bold]UID {self.uid}[/bold] ' \
                           f'\[{self.dendrite.receptor_pool.external_ip}] ' \
                           f'({self.wallet.name}:[bold]{self.wallet.coldkeypub.ss58_address[:7]}[/bold]/' \
                           f'{self.config.wallet.hotkey}:[bold]{self.wallet.hotkey.ss58_address[:7]}[/bold])'
@@ -329,6 +329,7 @@ class neuron:
                             f'[white]\[{step_time:.2f}s] step {epoch_steps} ({self.global_step} global)[/white]'
 
             columns = [('UID', 'uid', '{:.0f}'),
+                       ('Upd', 'updates', '{:.0f}'),
                        ('Route', 'routing_score', '{:.3f}'),
                        ('mShap', 'shapley_values_min', '{:.0f}'),
                        ('Loss', 'loss', '{:.2f}'),
@@ -346,8 +347,8 @@ class neuron:
             for col, _, _ in columns:
                 table.add_column(col)
 
-            rows = [[txt.format(s[key]) for _, key, txt in columns] for s in stats]
-            rows = sorted(rows, reverse=True, key=lambda _row: int(_row[2]))  # sort according to mShap column
+            rows = [[txt.format(self.server_stats[s['uid']]) for _, key, txt in columns] for s in stats]
+            rows = sorted(rows, reverse=True, key=lambda _row: int(_row[3]))  # sort according to mShap column
 
             for row in rows:
                 table.add_row(*row)
