202bf482fcdf8302cade078b8f10282a3fb796ec
opentaco <opentaco@protonmail.com>
2022-07-11 15:06:10 +0200

    Fix zeroing_keys use in neuron_stats_update in core_validator

--- bittensor/_neuron/text/core_validator/__init__.py
+++ bittensor/_neuron/text/core_validator/__init__.py
@@ -441,7 +441,7 @@ class neuron:
                 if uid in self.neuron_stats:
                     del self.neuron_stats[uid]
 
-    def neuron_stats_update(self, neuron_stats: Dict[int, Dict[int, Any]]):
+    def neuron_stats_update(self, neuron_stats: Dict[int, Dict[str, Any]]):
         r""" Updates self.neuron_stats with new individual dictionaries per uid.
         """
         for _uid, _stats in neuron_stats.items():
@@ -455,13 +455,14 @@ class neuron:
                 stats.setdefault('updates!', 0)  # number of EMA zeroing updates init to zero
 
             for key in self.zeroing_keys:
-                if key in stats:
-                    if key in _stats:
-                        stats[key + '!'] = (1 - self.alpha) * stats[key + '!'] + self.alpha * _stats[key + '!']
+                zkey = key + '!'
+                if zkey in stats:
+                    if zkey in _stats:
+                        stats[zkey] = (1 - self.alpha) * stats[zkey] + self.alpha * _stats[zkey]
                     else:
-                        stats[key + '!'] = (1 - self.alpha) * stats[key + '!']  # + self.alpha * 0
+                        stats[zkey] = (1 - self.alpha) * stats[zkey]  # + self.alpha * 0
                 else:
-                    stats.setdefault(key + '!', 0.)
+                    stats.setdefault(zkey, 0.)
 
             # === EMA normal update ===
             # If synapse responsive push available values into EMA for normal update.
