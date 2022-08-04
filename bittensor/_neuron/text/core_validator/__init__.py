bc3a470bee87d7f12cd90e90d0d3b807c48c75fc
Thebes <jake@bittensor.com>
2022-08-04 14:13:51 -0400

    minor error

--- bittensor/_neuron/text/core_validator/__init__.py
+++ bittensor/_neuron/text/core_validator/__init__.py
@@ -431,7 +431,7 @@ class neuron:
             self.prometheus_info.info( {'block': str(current_block) } )
             for uid, vals in self.neuron_stats.items():
                 for key in vals:
-                    if key not in self.prometheus_stats[uid]:
+                    if key not in self.prometheus_stats:
                         safe_promo_key = key.replace("!", "X").replace("~","Z")
                         self.prometheus_stats[key] = Summary('validator_stats_{}'.format(safe_promo_key), 'validator_stats_{}'.format(safe_promo_key), ['uid'])
                     self.prometheus_stats[key].labels( str(uid) ).observe( vals[key] )
