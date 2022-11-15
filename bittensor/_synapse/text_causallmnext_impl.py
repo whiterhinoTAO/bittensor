393624e9cdd802558d855330a4ffa517c1e2ea12
opentaco <opentaco@protonmail.com>
2022-11-15 11:22:56 +0200

    Update tokenizer utilities
    Add some additional unravel clamping to probabilities.

--- bittensor/_synapse/text_causallmnext_impl.py
+++ bittensor/_synapse/text_causallmnext_impl.py
@@ -125,7 +125,7 @@ class TextCausalLMNext(Synapse):
 
         atol = 1e-6  # absolute tolerance
         if (forward_response_tensor < -atol).any():
-            print(forward_response_tensor)
+            print(forward_response_tensor, forward_response_tensor[forward_response_tensor < -atol])
             raise ValueError("forward_response_tensor not allowed to be negative.")
 
     def check_backward_request_gradient(self, forward_request_tensor, backward_request_gradient):
