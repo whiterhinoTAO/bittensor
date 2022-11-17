af78d957ca6453bf9a797f2ee1476c827c410829
Carro <robertmyers@hey.com>
2022-11-17 13:38:23 -0600

    added ddp_server to __init__

--- bittensor/_neuron/__init__.py
+++ bittensor/_neuron/__init__.py
@@ -20,12 +20,15 @@ __version__ = '0.0.0'
 version_split = __version__.split(".")
 __version_as_int__ = (100 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
 
-from .text import core_validator, core_server
-__all_neurons__ =  {  
+from .text import core_validator, core_server, ddp_server
+__all_neurons__ =   {  
                      'text_core_validator': core_validator.neuron,
-                     'core_server': core_server.neuron}
+                     'core_server': core_server.neuron,
+                     'ddp_server': ddp_server.neuron
+                    }
 
 __text_neurons__ =  { 
                      'core_validator': core_validator.neuron,
-                     'core_server': core_server.neuron
-                     }
+                     'core_server': core_server.neuron,
+                     'ddp_server': ddp_server.neuron
+                    }
