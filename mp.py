67d6b2f6ae070483b199ca6842bbcd1b731af040
Carro <robertmyers@hey.com>
2022-11-19 19:14:11 -0600

    attempting to build my own mp

--- mp.py
+++ mp.py
@@ -1,16 +1,18 @@
+import pdb
 import bittensor as bt
 
 from transformers import AutoModelForCausalLM
-from parallelformers import parallelize
+
+
+# if __name__ == "__main__":
+model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-neo-125M")
+
+tokenizer = bt.tokenizer()
+
+def split_models(model, num_gpus: int):
+    pdb.set_trace()
+
 
 
 if __name__ == "__main__":
-    model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-neo-125M")
-    parallelize(model, num_gpus=4, fp16=True)
-
-    tokenizer = bt.tokenizer()
-
-    inputs_ids = tokenizer.encode("Hello, my dog is cute", return_tensors="pt")
-
-    outputs = model(inputs_ids)
-    print(outputs)
+    split_models(model, 1)
