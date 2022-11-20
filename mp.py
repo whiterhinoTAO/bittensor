261b42cb15f442ea6b58d3dd5e3145421fba32c1
Carro <robertmyers@hey.com>
2022-11-19 19:17:30 -0600

    attempting to build my own mp

--- mp.py
+++ mp.py
@@ -10,6 +10,14 @@ model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-neo-125M")
 tokenizer = bt.tokenizer()
 
 def split_models(model, num_gpus: int):
+    layers = model.transformer.h
+    layers_per_gpu = layers // num_gpus
+    
+    # a for loop that adds the layers to the gpu with .to(device)
+    for i in range(len(layers)):
+        # add the layer to the gpu
+        layers[i].to(f"cuda:{i // layers_per_gpu}")
+
     pdb.set_trace()
 
 
