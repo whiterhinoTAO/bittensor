745fe3c3a5a58f3d1903b025b36dd0e748c35411
Carro <robertmyers@hey.com>
2022-11-21 13:45:26 -0600

    testing

--- mp.py
+++ mp.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
     inputs = tokenizer("the dog is cute", return_tensors="pt")
 
     start_time = time.time()
-    outputs = pre_model.generate(**inputs, do_sample=True, max_length=256, top_k=50, top_p=0.95, num_return_sequences=1)
+    outputs = pre_model(**inputs)
     end_time = time.time()
     # target_output = 
 
