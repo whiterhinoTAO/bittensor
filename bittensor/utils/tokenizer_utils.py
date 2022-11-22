fixed response mix and loss calculation

bittensor/utils/tokenizer_utils.py
@@ -916,7 +916,7 @@ def unravel_topk_token_phrases(compact_topk: torch.Tensor, topk: int, ignore_ind
 def phrase_cross_entropy(target_phrases: Union[List[List[int]], torch.Tensor],
                          topk_tensor: torch.Tensor,
                          ignore_index: int = -100, reduce=True, reduction='mean',
-                         vocab_size_min: int = 50257) -> Tuple[torch.Tensor, torch.Tensor]:
+                         vocab_size_min: int = 50257, p = False) -> Tuple[torch.Tensor, torch.Tensor]:
     r"""
     Calculates the cross entropy of a phrase prediction against a target phrase, so that this is a multi-token
     extension of typical cross entropy calculated for next token prediction.
@@ -974,7 +974,7 @@ def phrase_cross_entropy(target_phrases: Union[List[List[int]], torch.Tensor],
             target_phrase = torch.tensor(target_phrases[b])
 
         match = (topk_tokens[b, :, 0] == target_phrase[0].item())  # bool where first tokens match (validation token)
-        if match.sum() > 0:
+        if match.sum() > 0 and n_topk_probs[b, match].sum() > 0:
             val_probs[b] = n_topk_probs[b, match].sum()  # accumulate all matches
         else:  # no matches
             val_probs[b] = n_floor_probs[b]  # assume match is in non-topk tokens with avg floor_prob
@@ -989,15 +989,20 @@ def phrase_cross_entropy(target_phrases: Union[List[List[int]], torch.Tensor],
             match = (topk_tokens[b, :, :check_len] == target)
             match_idx = torch.where(match.sum(dim=-1) == check_len)[0]  # phrase indices which match sub target
 
-            if len(match_idx):  # at least one match
+            if len(match_idx) and n_topk_probs[b, match_idx].sum() > 0:  # at least one match
                 match_probs[b] += n_topk_probs[b, match_idx].sum()  # accumulate all matches
             else:  # no matches
                 match_probs[b] += n_floor_probs[b]  # assume match is in non-topk tokens with avg floor_prob
+
+            if p and match_probs[b] == 0:
+                print(len(match_idx), match[match_idx], n_topk_probs[b, match_idx].sum(), n_floor_probs[b])                
 
     old_val_probs = val_probs.clone()
     val_probs = torch.clamp(val_probs, 0, 1)  # [batch_size] ensure 0 <= total probability <= 1
     loss_val = - torch.log(val_probs + 1e-40)  # [batch_size] calculate cross entropy loss
 
+
+    old_match_probs = match_probs.clone()
     match_probs = torch.clamp(match_probs, 0, 1)  # [batch_size] ensure 0 <= total probability <= 1
     loss = - torch.log(match_probs + 1e-40)  # [batch_size] calculate cross entropy loss
 
@@ -1008,18 +1013,22 @@ def phrase_cross_entropy(target_phrases: Union[List[List[int]], torch.Tensor],
         loss = getattr(loss, reduction)()
         if loss.numel() > 1:
             raise ValueError(f'phase_cross_entropy(): Expected reduction to scalar, obtained {loss.shape} instead.')
-    print (
-        'phrase cross entropy \n', topk_tensor[0, :10, :], 
-        'topk tokens: ', topk_tokens.sum(), 
-        'topk_probs max mins: ', topk_probs.max(), topk_probs.min(),
-        'topk_probs.sum(dim=-1): ', topk_probs.sum(dim=-1), 
-        'max(0, vocab_size_min - topk) * floor_probs: ', max(0, vocab_size_min - topk) * floor_probs, 
-        'floor probs: ',floor_probs, 
-        'n_floor_probs: ', n_floor_probs, 
-        'match sum: ',match.sum(), 
-        'val_probs: ', old_val_probs, 
-        'loss',loss
-    )
+    
+    if p:
+        topk_tensor_sorted = topk_tensor[0, : , :][topk_tensor[0, : , 0].sort(descending= True)[1]]
+        print (
+            '\nphrase cross entropy\n', topk_tensor_sorted[: 10, :], 
+            '\ntopk tokens: ', topk_tokens.sum(), 
+            '\ntopk_probs max mins: ', topk_probs.max(), topk_probs.min(),
+            '\ntopk_probs.sum(dim=-1): ', topk_probs.sum(dim=-1), 
+            '\nmax(0, vocab_size_min - topk) * floor_probs: ', max(0, vocab_size_min - topk) * floor_probs, 
+            '\nfloor probs: ',floor_probs, 
+            '\nn_floor_probs: ', n_floor_probs, 
+            '\nmatch sum: ',match.sum(), 
+            '\nval_probs: ', old_val_probs, 
+            '\nmatch probs: ', old_match_probs,
+            '\nloss ',loss
+        )
 
     return loss_val, loss
 

train_memory.py
@@ -209,9 +209,9 @@ class Nucleus(nn.Module):
         for f in futures: del f
         return [ r.to(self.config.nucleus.device) for r in results], successes
 
-    def cal_loss(self, inputs, query_response, validation_len = 8):
+    def cal_loss(self, inputs, query_response, validation_len = 8, p = False):
         inputs_nxt = inputs[..., -validation_len:]
-        _losses_val, _losses = phrase_cross_entropy(inputs_nxt, query_response, reduce=True)
+        _losses_val, _losses = phrase_cross_entropy(inputs_nxt, query_response, reduce=True, p = p)
         return _losses
 
     def mix_response(self, response_success, normalized_topk_routing_scores):
@@ -225,26 +225,20 @@ class Nucleus(nn.Module):
             response = torch.zeros(batch_size, bittensor.__vocab_size__ , 2)
             response[:, :, 1] = all_logits.repeat(batch_size, 1)
 
-            org = r[0][0, : -1, :]
-            print('org', org[org[:, 0].sort(descending = True)[1]][: 10, :])
             
             for batch in range(batch_size):
-                index = r[0][batch, : -1, 1].long()
-                prob = r[0][batch, : -1, 0] 
-                response[batch, :, 0] = response[batch, :, 0].index_add_(0, index, prob)
-
-                if batch == 0:
-                    print_response = response[batch]
-                    print('print response', print_response[print_response[:, 0].sort(descending = True)[1]][: 10, :])
+                r_batch = r[0][batch, : -1, :]
+                r_batch_sorted = r_batch[r_batch[:, 0].sort(descending = False)[1]]
+                index = r_batch_sorted[:, 1].long()
+                prob = r_batch_sorted[:, 0] 
+                response[batch, index, 0] = prob
             
             mixed_response[:, :-1, 0] += w * response[:, :, 0]
 
-        # reduced_mixed_response = torch.zeros(batch_size, topk + 1  , 2)
         for batch in range(batch_size):
-            # mixed_batch = mixed_response[batch, :, :][mixed_response[batch, :, 0].sort(descending=True)[1]][:topk, :]
-            # floor_prob = (1 - sum(mixed_batch[:, 0])) / (bittensor.__vocab_size__ - topk)
-            # reduced_mixed_response[batch, :-1, :] = mixed_batch
-            mixed_response[batch, -1, :] = torch.tensor([[0, -1]])
+            mixed_batch = mixed_response[batch, :, :][mixed_response[batch, :, 0].sort(descending=True)[1]][:topk, :]
+            floor_prob = (1 - sum(mixed_batch[:, 0])) / (bittensor.__vocab_size__ - topk)
+            mixed_response[batch, -1, :] = torch.tensor([[floor_prob, -1]])
 
         # return reduced_mixed_response
         return mixed_response
@@ -301,33 +295,18 @@ class Nucleus(nn.Module):
         response_success = [r for r, op in list(zip(responses, return_ops)) if op == 1]
         time_success = [t for t, op in list(zip(times, return_ops)) if op == 1]
         normalized_topk_routing_scores = topk_routing_scores[successes]/topk_routing_scores[successes].sum()
-        zero_response = self.mix_response([response_success[0]], torch.tensor([1]))
-        mixed_ind_responses = [self.mix_response([r], torch.tensor([1])) for r in response_success]
         mixed_response = self.mix_response(response_success, normalized_topk_routing_scores)
         averaged_response = self.mix_response(response_success, torch.ones_like(normalized_topk_routing_scores) / len(normalized_topk_routing_scores))
 
         # Compute loss.
-        loss_zero = self.cal_loss(inputs, zero_response, self.config.nucleus.validation_len)
-        loss_zero_org = self.cal_loss(inputs, response_success[0][0][:, : , :2], self.config.nucleus.validation_len)
         loss_routing = self.cal_loss(inputs, mixed_response, self.config.nucleus.validation_len)
         loss_routing_baseline = self.cal_loss(inputs, averaged_response, self.config.nucleus.validation_len)
         loss_model_baseline = self.cal_loss(inputs, response_baseline, self.config.nucleus.validation_len)
-        print('=================================================')
         losses = torch.tensor([self.cal_loss(inputs, r[0][:, :, :2], self.config.nucleus.validation_len) for r in response_success])
-        print('=================================================')
-        mixed_ind_losses = [self.cal_loss(inputs, r, self.config.nucleus.validation_len) for r in mixed_ind_responses]
-        print('=================================================')
-        fake_response = torch.zeros_like(response_success[0][0][:, : , :2])
-        fake_response[:, -2 , 0] = torch.ones_like(fake_response[:, -1 , 0]) * -46144.7734
-        fake_response[:, -1 , 0] = torch.ones_like(fake_response[:, -1 , 0])
-        lol = self.cal_loss(inputs, fake_response, self.config.nucleus.validation_len)
+        print(list(zip(uids.tolist(), losses.tolist())))
         loss_min = min(losses)
-        
-        print(losses, mixed_ind_losses)
 
         stats = {
-            'loss/zero': loss_zero,
-            'loss/zero_org': loss_zero_org,
             'loss/min': loss_min,
             'loss/count': len(losses),
             'loss/average': sum(losses) / len(losses),
@@ -339,6 +318,28 @@ class Nucleus(nn.Module):
             'stat/batch_time': time.time() - start_time,
             'stat/qps': qps,
         }
+        print(stats)
+
+        if loss_routing > 10:
+            print("==================================================")
+            for r in response_success[:3]:
+                ind_r = self.mix_response([r], torch.tensor([1]))
+                org_loss = self.cal_loss(inputs, r[0][:, :, :2], self.config.nucleus.validation_len, p = True)
+                ind_loss = self.cal_loss(inputs, ind_r, self.config.nucleus.validation_len, p = True)
+
+                org = r[0][0, : , :]
+                mixed = ind_r[0, :, :]
+                print('org \n', org[org[:, 0].sort(descending = True)[1]][: 10, :2])
+                print('ind r \n', mixed[mixed[:, 0].sort(descending = True)[1]][:10, :2])
+                print('org tail\n', org[org[:, 0].sort(descending = True)[1]][-20:, :2])
+                ind_r_tail = mixed[mixed[:, 0].sort(descending = True)[1]]
+                print('ind r tail \n', ind_r_tail[ind_r_tail[:, 0].nonzero()][-20:])
+                print('org token \n', org[org[:, 1].sort()[1]][:10, :2])
+                print('ind r token \n', mixed[mixed[:, 1].sort()[1]][:10, :2])
+                print(org_loss, ind_loss)
+            print("==================================================")
+
+
         
         # Return.
         return stats, successes, routing_score
@@ -475,10 +476,15 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=config.max_workers) as ex
         topk_vals, topk_uids = average_scores.topk( config.nucleus.n_queried )
 
         if config.use_wandb:
-            scores_log = { 'score/' + str(k) :v.item() for k, v in enumerate(average_scores)}
+            scores_log = {}
+            for k, v in enumerate(average_scores):
+                if k in target_uids:
+                    scores_log['score/target/' + str(k)] = v.item()
+                else:
+                    scores_log['score/' + str(k)] = v.item()
+
             stats['stat/success'] = sum(successes) / len(successes)
             stats['stat/num_success'] = sum(successes) 
-            print(stats)
             wandb.log( {**stats, **scores_log}, step=ci)
 
 # Measure state after.