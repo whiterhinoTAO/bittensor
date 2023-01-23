81079fec8dfc9c101b16c6fa2e7c8d1c8232cc4f
Carro <robertmyers@hey.com>
2023-01-23 14:57:43 -0600

    forgot some functions

---
+++ bittensor/testing_ds.py
@@ -0,0 +1,27 @@
+import os
+import deepspeed
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+
+world_size = int(os.getenv('WORLD_SIZE', '1'))
+local_rank = int(os.getenv('LOCAL_RANK', '0'))
+device = torch.device("cuda", local_rank)
+
+
+model = AutoModelForCausalLM.from_pretrained("gpt2")
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+
+ds_engine = deepspeed.init_inference(model,
+                         mp_size=world_size,
+                         dtype=torch.half,
+                        #  replace_method='auto',
+                        #  replace_with_kernel_inject=True
+                         )
+
+model_engine = ds_engine.module
+
+input_ids = tokenizer.encode("Hello, my dog is cute", return_tensors='pt').to(device)
+outputs = model_engine.generate(input_ids, max_length=128, do_sample=True)
+outputs
+import code; code.interact(local=dict(globals(), **locals()))
