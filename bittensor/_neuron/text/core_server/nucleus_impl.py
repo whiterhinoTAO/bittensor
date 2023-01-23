1afa0d8aa9f4530faf4db0ebf1138f37a55a0310
Carro <robertmyers@hey.com>
2023-01-23 16:34:26 -0600

    forgot some functions

--- bittensor/_neuron/text/core_server/nucleus_impl.py
+++ bittensor/_neuron/text/core_server/nucleus_impl.py
@@ -524,7 +524,7 @@ class server(torch.nn.Module):
         parser.add_argument('--config', type=str, help='If set, defaults are overridden by passed file.')
 
         # deepspeed arguments
-        parser.add_argument('--deepspeed.deepspeed_config', type=str, help='If set, defaults are overridden by passed file.', default='~/.bittensor/bittensor/bittensor/_neuron/text/core_server/ds_config.json')
+        parser.add_argument('--deepspeed.deepspeed_config', type=str, help='If set, defaults are overridden by passed file.', default='/home/ubuntu/.bittensor/bittensor/bittensor/_neuron/text/core_server/ds_config.json')
         parser.add_argument('--deepspeed.local_rank', type=int, default=-1, help='Local rank of the process.')
         parser.add_argument('--deepspeed.world_size', type=int, default=-1, help='Number of GPUs to use.')
         
