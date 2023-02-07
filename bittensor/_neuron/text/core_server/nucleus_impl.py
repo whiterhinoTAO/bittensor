5a546de78c3395c28cbfeb5b9cd37f636855b2d6
Eugene <etesting007@gmail.com>
2023-02-07 11:49:06 -0800

    devices

--- bittensor/_neuron/text/core_server/nucleus_impl.py
+++ bittensor/_neuron/text/core_server/nucleus_impl.py
@@ -454,6 +454,7 @@ class server(torch.nn.Module):
 
         def _forward(tokens,training = False, _model_output=model_output):
             if _model_output is None:
+                print(tokens['input_ids'].device, tokens['attention_mask'].device)
                 _model_output = self.pre_model(input_ids=tokens['input_ids'],
                                                attention_mask=tokens['attention_mask'],
                                                output_hidden_states=True)
